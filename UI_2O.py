10c60c7c2854460cb326ecb1a2720367d8bf0794
ramganesh100 <ganeshramdsc@gmail.com>
2026-03-30 17:38:52 +0530

    hi

--- UI_2O.py
+++ UI_2O.py
@@ -5,7 +5,7 @@ import datetime
 import time
 import os
 import random
-#from mutagen.mp3 import MP3
+from mutagen.mp3 import MP3
 
 # =========================================================
 # INITIAL SETUP
@@ -357,6 +357,20 @@ ctk.CTkLabel(
     textvariable=clock_var,
     font=("Arial", 24)
 ).pack(side="right", padx=20)
+
+# Start of exit full screen mode
+exit_btn = ctk.CTkButton(
+    top_frame,
+    text="Exit Full Screen",font=("Arial", 18),
+    command=exit_fullscreen,
+    width=140,
+    height=50,
+    fg_color="#FF4C4C",
+    hover_color="#CC0000"
+)
+exit_btn.pack(side="right", padx=10)
+# End of full screen mode
+
 
 theme_button = ctk.CTkButton(
     top_frame,
